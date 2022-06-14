b54fb5368482e68dc730978d3abdc9569cf1d96b
@BagliettoAron <aronbaglietto88@gmail.com>
2022-06-14 16:55:23 +0200

    Add: e-shop products

--- index.php
+++ index.php
@@ -5,10 +5,24 @@ error_reporting(E_ALL);
 ?>
 
 <?php 
-    require_once __DIR__ . '/Product.php';
+require_once __DIR__ . '/Cibo.php';
+require_once __DIR__ . '/Gioco.php';
+require_once __DIR__ . '/Collare.php';
+require_once __DIR__ . '/Cucce.php';
 
-    $cibo = new Product(20, 200, 'dog', 5 );
+    
+
+    $crocchette_cane = new Cibo(20, 200, 'dog', 5, 'damp' );
+
+    $collare_gatto = new Collare(19.99, 'cat', 2);
+
+    $cuccia_furetto = new Cucce(35, 'furetto', 3);
+
+    $gioco_criceto = new Gioco(15, 'criceto', 4);
+
+
+
     echo '<pre>';
-    var_dump($cibo);
+    var_dump($gioco_criceto);
     echo '</pre>';
 ?>
